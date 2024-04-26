fbb4fee13fe4375762f5e7e30811ce8727a94998
awesomebunny <awesomebunny@bellsouth.net>
2024-04-26 14:12:52 -0400

    End of part 1.c

--- part1-learning/src/App.jsx
+++ part1-learning/src/App.jsx
@@ -9,21 +9,21 @@ const Button = ({onClick, text}) => <button onClick = {onClick}>{text}</button>
 
 const App = () => {
 
-  const [ counter, setCounter ] = useState(0)
+  const [ counter, setCounter ] = useState(0);
   console.log('rendering with counter value', counter);
 
   const increaseByOne = () => {
     console.log('increasing, value before', counter);
     setCounter(counter + 1);
-  }
+  };
   const decreaseByOne = () => {
     console.log('decreasing, value before', counter);
     setCounter(counter - 1);
-  }
+  };
   const setToZero = () => {
     console.log('resetting to zero, value before', counter);
     setCounter(0);
-  }
+  };
 
   // console.log('rendering...', counter)
 
@@ -34,7 +34,7 @@ const App = () => {
       <Button onClick = {setToZero} text = "zero"/>
       <Button onClick = {decreaseByOne} text = "minus"/>
     </div>
-  )
-}
+  );
+};
 
 export default App
