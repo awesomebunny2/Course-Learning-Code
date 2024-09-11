bfaeab0455fca4995ec8e39d6200c6e60d52390b
awesomebunny <awesomebunny@bellsouth.net>
2024-09-11 16:28:51 -0400

    push for hosting

--- part3-learning/index.js
+++ part3-learning/index.js
@@ -1,81 +1,134 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
+const cors = require("cors");
+
+app.use(cors());
+
+//#region NOTES --------------------------------------------------------------------------------------------------------------------------------------
+
+    let notes = [
+        {
+            id: "1",
+            content: "HTML is easy",
+            important: true
+        },
+        {
+            id: "2",
+            content: "Browser can execute only JavaScript",
+            important: false
+        },
+        {
+            id: "3",
+            content: "GET and POST are the most important methods of HTTP protocol",
+            important: true
+        }
+    ];
+
+//#endregion -----------------------------------------------------------------------------------------------------------------------------------------
 
 
-let notes = [
-    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-];
+//#region MIDDLEWARE FUNCTIONS -----------------------------------------------------------------------------------------------------------------------
+    
+    const requestLogger = (request, response, next) => {
+        console.log('Method:', request.method)
+        console.log('Path:  ', request.path)
+        console.log('Body:  ', request.body)
+        console.log('---')
+        next()
+    };
+
+    const unknownEndpoint = (request, response) => {
+        response.status(404).send({error: "Unknown Endpoint..."});
+    };
+
+//#endregion -----------------------------------------------------------------------------------------------------------------------------------------
 
 
-app.get("/", (request, response) => {
-    response.send("<h1>Hello World!</h1>");
-});
+//#region CALL MIDDLEWARE ----------------------------------------------------------------------------------------------------------------------------
+    
+    app.use(express.json());
+    app.use(requestLogger);
 
-app.get("/api/notes", (request, response) => {
-    response.json(notes);
-});
+//#endregion -----------------------------------------------------------------------------------------------------------------------------------------
 
-app.get("/api/notes/:id", (request, response) => {
-    const id = request.params.id;
-    const note = notes.find(note => note.id === id);
 
-    if (note) {
+//#region FUNCTIONS ----------------------------------------------------------------------------------------------------------------------------------
+
+    const generateId = () => {
+        const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0;
+        return String(maxId + 1);
+    };
+
+//#endregion -----------------------------------------------------------------------------------------------------------------------------------------
+
+
+//#region ROUTES -------------------------------------------------------------------------------------------------------------------------------------
+
+    app.get("/", (request, response) => {
+        response.send("<h1>Hello World!</h1>");
+    });
+
+    app.get("/api/notes", (request, response) => {
+        response.json(notes);
+    });
+
+    app.get("/api/notes/:id", (request, response) => {
+        const id = request.params.id;
+        const note = notes.find(note => note.id === id);
+
+        if (note) {
+            response.json(note);
+        } else {
+            response.statusMessage = `There is no note with the id of ${id} in the database`;
+            response.status(404).end();
+        };
+    });
+
+    app.delete("/api/notes/:id", (request, response) => {
+        const id = request.params.id;
+        notes = notes.filter(note => note.id !==id);
+        response.status(204).end();
+    });
+
+
+
+    app.post("/api/notes", (request, response) => {
+        const body = request.body;
+
+        if (!body.content) {
+            return response.status(400).json({
+                error: "Body Content Missing..."
+            });
+        };
+
+        const note = {
+            content: body.content,
+            important: Boolean(body.important) || false,
+            id: generateId()
+        };
+
+        notes = notes.concat(note);
+
+        console.log(note);
         response.json(note);
-    } else {
-        response.statusMessage = `There is no note with the id of ${id} in the database`;
-        response.status(404).end();
-    };
-});
+    });
 
-app.delete("/api/notes/:id", (request, response) => {
-    const id = request.params.id;
-    notes = notes.filter(note => note.id !==id);
-    response.status(204).end();
-});
+//#endregion -----------------------------------------------------------------------------------------------------------------------------------------
 
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0;
-    return String(maxId + 1);
-};
 
-app.post("/api/notes", (request, response) => {
-    const body = request.body;
+//#region CALL MIDDLEWARE ----------------------------------------------------------------------------------------------------------------------------
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: "Body Content Missing..."
-        });
-    };
+    app.use(unknownEndpoint);
 
-    const note = {
-        content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId()
-    };
+//#endregion -----------------------------------------------------------------------------------------------------------------------------------------
 
-    notes = notes.concat(note);
 
-    console.log(note);
-    response.json(note);
-});
+//#region APP LISTEN ---------------------------------------------------------------------------------------------------------------------------------
 
-const PORT = 3001;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+
+//#endregion -----------------------------------------------------------------------------------------------------------------------------------------
